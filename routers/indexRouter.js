3e3919682d9a6aa64ac2517769ac6e64fcb4c20a
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-07-11 18:16:29 +0200

    Create the views for login and signup

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -2,7 +2,23 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.send("index");
+  res.render("index");
+});
+
+indexRouter.get("/sign-up", (req, res) => {
+  res.render("sign-up");
+});
+
+indexRouter.post("/sign-up", (req, res) => {
+  res.redirect("/sign-up");
+});
+
+indexRouter.get("/log-in", (req, res) => {
+  res.render("log-in");
+});
+
+indexRouter.post("/log-in", (req, res) => {
+  res.redirect("/log-in");
 });
 
 module.exports = indexRouter;
