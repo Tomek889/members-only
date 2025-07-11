c75e439a02fcaf9bbd70e803c4d35765df0ba781
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-07-11 15:38:49 +0200

    Create the indexRouter

---
+++ app.js
@@ -0,0 +1,18 @@
+const path = require("node:path");
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const indexRouter = require("./routers/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "f", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+app.listen(3000, () => console.log("App listening on port 3000."));
